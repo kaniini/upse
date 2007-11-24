fe7fdad7b7c451c78ff6e846a16ec14465e55236
William Pitcock <nenolod@atheme.org>
2007-11-24 05:36:59 -0600

    Add experimental dynamic recompiler for x64, based on PCSX2. Incomplete.

--- src/libupse/upse-types.h
+++ src/libupse/upse-types.h
@@ -31,11 +31,13 @@ typedef int8_t s8;
 typedef int16_t s16;
 typedef int32_t s32;
 typedef int64_t s64;
+typedef intptr_t sptr;
 
 typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
+typedef uintptr_t uptr;
 
 #ifdef __GNUC__
 # define PACKSTRUCT	__attribute__ ((packed))
