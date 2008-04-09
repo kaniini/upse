00f6a8651218bff09aaf16f7d91363786753fff6
William Pitcock <nenolod@atheme.org>
2008-04-09 14:41:05 -0500

    volume adjustment

--- src/libupse/upse_ps1_spu_base.c
+++ src/libupse/upse_ps1_spu_base.c
@@ -571,8 +571,8 @@ int SPUasync(u32 cycles)
 	}
 
 	/* fix dynamic range. */
-	sl = (sl * volmul) >> 10;
-	sr = (sr * volmul) >> 10;
+	sl = (sl * volmul) >> 8;
+	sr = (sr * volmul) >> 8;
 
 	if (sl > 32767)
 	    sl = 32767;
@@ -746,7 +746,7 @@ int SPUopen(void)
     memset((void *) s_chan, 0, (MAXCHAN + 1) * sizeof(SPUCHAN));
     pSpuIrq = 0;
 
-    iVolume = 160;		//85;
+    iVolume = 255;		//85;
     SetupStreams();		// prepare streaming
 
     bSPUIsOpen = 1;
