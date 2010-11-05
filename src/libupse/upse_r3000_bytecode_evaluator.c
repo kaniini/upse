96dcd915bae18a1a0730d4b046508f85d9cbafe4
William Pitcock <nenolod@atheme.org>
2010-11-05 02:54:46 -0500

    r3k: Debug HLE calls.

--- src/libupse/upse_r3000_bytecode_evaluator.c
+++ src/libupse/upse_r3000_bytecode_evaluator.c
@@ -19,6 +19,7 @@
 
 #include <stdlib.h>
 
+#define UPSE_DEBUG
 #include "upse-internal.h"
 
 extern void upse_ps2_iop_call(uint32_t callnum);
@@ -1023,6 +1024,7 @@ static void psxCOP0()
 
 static void psxHLE()
 {
+    _DEBUG("HLE - PC: 0x%lx", upse_r3000_cpu_regs.pc);
     psxHLEt[upse_r3000_cpu_regs.code & 0xff] ();
 }
 
